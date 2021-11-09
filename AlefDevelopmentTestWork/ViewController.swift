af90d4b9fee0489de3eee0c0f5b5b280ad46b84f
Artem <nwlf.420@gmail.com>
2021-11-09 11:46:57 +0300

    Initial Commit

---
+++ AlefDevelopmentTestWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlefDevelopmentTestWork
+//
+//  Created by pro2017 on 09/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
